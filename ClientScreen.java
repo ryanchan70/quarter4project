f5e32ca769466bd4cf39d86e6df6e8f008ec9fc3
ryan <91448275+ryanchan70@users.noreply.github.com>
2023-05-18 09:43:43 -0700

    clientscreen file: beta obstacle movement at 50 fps

--- ClientScreen.java
+++ ClientScreen.java
@@ -10,10 +10,8 @@ public class ClientScreen extends JPanel implements ActionListener {
     private Client client;
     private JButton startButton;
     private int numReady, numClients;
-    private boolean ready;
-    private boolean obstacleSent;
-    private boolean startGame;
-    Obstacle obstacle = new Obstacle(500,500,50,50);
+    private boolean ready, startGame;
+    private Obstacle[] obstacles;
 
     public ClientScreen() {
         startGame = false;
@@ -24,13 +22,16 @@ public class ClientScreen extends JPanel implements ActionListener {
 
         ready = false;
 
+        obstacles = new Obstacle[3];
+        for (int i = 0; i < obstacles.length; i++) {
+            obstacles[i] = new Obstacle();
+        }
+
         setLayout(null);
         setFocusable(true);
     }
 
-    public Dimension getPreferredSize() {
-        return new Dimension(800, 600);
-    }
+
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -43,13 +44,15 @@ public class ClientScreen extends JPanel implements ActionListener {
             g.setFont(new Font("SansSerif", Font.BOLD, 20));
             g.drawString("Please wait for all players to press start.", 170, 175);
             g.drawString(numReady + "/" + numClients + " players ready", 300, 250);
-        }
-        else {
-            g.setColor(Color.BLUE);
-            g.fillRect(obstacle.getX(), obstacle.getY(), obstacle.getHeight(), obstacle.getLength());
-
+        } else {
+            g.setColor(new Color(52, 168, 117));
+            for (int i = 0; i < obstacles.length; i++) {
+                g.fillRect(obstacles[i].getX(), obstacles[i].getY(), obstacles[i].getWidth(), obstacles[i].getHeight());
+                System.out.println(obstacles[i].getX() + " " + obstacles[i].getY());
+            }
         }
         System.out.println("clientscreen: " + numReady + " " + numClients);
+        repaint();
     }
 
     public void actionPerformed(ActionEvent e) {
@@ -64,13 +67,18 @@ public class ClientScreen extends JPanel implements ActionListener {
         this.numClients = numClients;
         if(numReady == numClients) {
             startGame = true;
+            client.send("STARTGAME"); //tells the serverthread to start game
             startButton.setVisible(false);
         }
         repaint();
     }
 
-    public void obstacleSent(){
-        this.obstacleSent = true;
+    public void updateObstacles(String[] obstacleInfo) {
+        // expected input format: obstacle1X obstacle1Y obstacle2X obstacle2Y, etc.
+        for (int i = 0; i < obstacleInfo.length; i += 2) {
+            obstacles[i/2].setX(Integer.parseInt(obstacleInfo[i]));
+            obstacles[i/2].setY(Integer.parseInt(obstacleInfo[i+1]));
+        }
     }
 
     public void setClient(Client client) {
@@ -86,5 +94,8 @@ public class ClientScreen extends JPanel implements ActionListener {
                 client.send("NOTREADY");
             }
         }
+    }
+    public Dimension getPreferredSize() {
+        return new Dimension(800, 600);
     }
 }
