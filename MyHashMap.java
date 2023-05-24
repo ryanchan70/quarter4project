3ea18dac69e96735b4259f726b6846ce82d57a1e
Rohan Singh <93063601+ComputerScienceGenius@users.noreply.github.com>
2023-05-24 11:47:40 -0700

    Create MyHashMap.java

---
+++ MyHashMap.java
@@ -0,0 +1,42 @@
+public class MyHashMap<K,V>{
+    private Object[] hashArray;
+    private int size;
+    private MyHashSet<K> keySet;
+
+    public MyHashMap(){
+        hashArray = new Object[100000];
+        size = 0;
+        keySet = new MyHashSet<>();
+    }
+
+    public V put(K a, V b){
+        V c = (V)(hashArray[a.hashCode()]);
+        hashArray[a.hashCode()] = b;
+        keySet.remove(a);
+        keySet.add(a);
+        if (c == null){
+            size++;
+        }
+        return c;
+    }
+
+    public V get(Object a){
+        V b = (V)(hashArray[a.hashCode()]);
+        return b;
+    }
+
+    public V remove(Object a){
+        V b = (V)(hashArray[a.hashCode()]);
+        hashArray[a.hashCode()] = null;
+        keySet.remove((K)(a));
+        return b;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public MyHashSet<K> keySet(){
+        return keySet;
+    }
+}
